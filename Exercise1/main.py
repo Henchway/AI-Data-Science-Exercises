b09a522c82c764f35e2aafab7b8aee03b0d4724e
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2021-04-12 19:13:32 +0200

    manhattan distance added to Node

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -22,13 +22,13 @@ def manhattan_distance(current_state: List[List[int]], goal: List[List[int]]):
     distance = 0
     for i, array in enumerate(current_state):
         for j, number in enumerate(array):
-            (row, column) = find_index(x=number, goal=goal)
+            (row, column) = find_index(x=number, matrix=goal)
             distance += abs(i - row) + abs(j - column)
     return distance
 
 
-def find_index(x, goal: List[List[int]]):
-    for row, i in enumerate(goal):
+def find_index(x, matrix: List[List[int]]):
+    for row, i in enumerate(matrix):
         try:
             column = i.index(x)
         except ValueError:
@@ -37,8 +37,8 @@ def find_index(x, goal: List[List[int]]):
     return -1
 
 
-def build_tree(current_state, depth, root=None, parent=None):
-    (row, col) = find_index(None, goal=current_state)
+def build_tree(current_state, depth, goal, root=None, parent=None):
+    (row, col) = find_index(None, matrix=current_state)
     if root is None:
         root = Node("root", array=current_state)
     if parent is None:
@@ -69,21 +69,28 @@ def build_tree(current_state, depth, root=None, parent=None):
         return node
 
     if row != 0 and parent.name != "down":
-        Node("up", parent=parent, array=add_up())
+        up = add_up()
+        manhattan = manhattan_distance(current_state=up, goal=goal)
+        Node("up", parent=parent, array=up, heuristic=manhattan)
 
     if row != len(current_state) - 1 and parent.name != "up":
-        Node("down", parent=parent, array=add_down())
+        down = add_down()
+        manhattan = manhattan_distance(current_state=down, goal=goal)
+        Node("down", parent=parent, array=down, heuristic=manhattan)
 
     if col != 0 and parent.name != "right":
-        Node("left", parent=parent, array=add_left())
+        left = add_left()
+        manhattan = manhattan_distance(current_state=left, goal=goal)
+        Node("left", parent=parent, array=left, heuristic=manhattan)
 
     if col != len(current_state[0]) - 1 and parent.name != "left":
-        Node("right", parent=parent, array=add_right())
+        right = add_right()
+        manhattan = manhattan_distance(current_state=right, goal=goal)
+        Node("right", parent=parent, array=right, heuristic=manhattan)
 
     for child in parent.children:
         if parent.depth < depth:
-            build_tree(current_state=child.array, depth=depth, root=root, parent=child)
-
+            build_tree(current_state=child.array, depth=depth, goal=goal, root=root, parent=child)
     return root
 
 
@@ -93,11 +100,9 @@ print(f"Manhattan Distance: {distance}")
 misplaced = count_misplaced(current_state=initial_state, goal=goal_state)
 print(f"Misplaces pieces: {misplaced}")
 
-tree = build_tree(current_state=initial_state, depth=9)
+tree = build_tree(current_state=initial_state, depth=9, goal=goal_state)
 for pre, fill, node in RenderTree(tree):
     print("%s%s" % (pre, node.name))
-
-
 
 # from ida import iterative_deepening_a_star
 # iterative_deepening_a_star(0, heuristic=manhattan_distance, start=initial_state, goal=goal_state)
